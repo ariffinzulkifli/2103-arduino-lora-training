866e19c517b33bef1bfa99b5d87493404b794f1b
Mohamad Ariffin Zulkifli <ariffinastute@live.com>
2021-03-23 11:25:22 +0800

    Update 02C_Running_LED_For_Loop.ino

--- 02C_Running_LED_For_Loop/02C_Running_LED_For_Loop.ino
+++ 02C_Running_LED_For_Loop/02C_Running_LED_For_Loop.ino
@@ -1,9 +1,9 @@
 void setup() {
-  for(int led = 7; led >= 10; led++) pinMode(led, OUTPUT);
+  for(int led = 7; led <= 10; led++) pinMode(led, OUTPUT);
 }
 
 void loop() {
-  for(int led = 7; led >= 10; led++){
+  for(int led = 7; led <= 10; led++){
     digitalWrite(led, HIGH);
     delay(500);
     digitalWrite(led, LOW);
